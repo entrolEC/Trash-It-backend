ade321f5bf93ba3eae2a01a690ddc7b1e8fd08c4
qoquma <washington7613@gmail.com>
2020-11-18 04:23:59 +0900

    image upload

--- trashcanMap/location/models.py
+++ trashcanMap/location/models.py
@@ -4,4 +4,4 @@ class Trashcan(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     address = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="location/media/image")
+    image = models.FileField(upload_to="location/media/image", max_length=300)
