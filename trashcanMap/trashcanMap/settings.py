2662f600e6ca5e4993d041467604d6f47e569aa1
qoquma <washington7613@gmail.com>
2020-12-05 03:42:08 +0900

    secret

--- trashcanMap/trashcanMap/settings.py
+++ trashcanMap/trashcanMap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+'''
 secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
@@ -33,7 +33,7 @@ def get_secret(setting, secrets=secrets):
     except KeyError:
         error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
-
+'''
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "m0j01kt7zdy!b&v1jdwf&f(*g7b7=imf%kz-wazgr8uy_t!+11"
