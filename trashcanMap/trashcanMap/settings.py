518784499a4defef979e0c9a533f58e433d35816
INK-Kano <user1205@naver.com>
2021-10-03 19:29:41 +0900

    change wsgi settings

--- trashcanMap/trashcanMap/settings.py
+++ trashcanMap/trashcanMap/settings.py
@@ -78,7 +78,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
