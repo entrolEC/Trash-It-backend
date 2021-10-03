a3b24cb77a0c4007b4f00d3706ec0cd4ed5e017d
INK-Kano <user1205@naver.com>
2021-10-03 19:04:14 +0900

    change wsgi setting

--- trashcanMap/trashcanMap/wsgi.py
+++ trashcanMap/trashcanMap/wsgi.py
@@ -15,3 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trashcanMap.settings')
 
 application = get_wsgi_application()
 
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
