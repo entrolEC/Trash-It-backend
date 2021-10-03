280e492541aef7e32585ba42d23dc4268d66c522
INK-Kano <user1205@naver.com>
2021-10-03 17:42:44 +0900

    change directory

--- trashcanMap/trashcanMap/settings.py
+++ trashcanMap/trashcanMap/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'location',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
 
     'corsheaders',
 ]
