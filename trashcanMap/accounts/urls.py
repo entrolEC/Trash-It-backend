48694ee40f2164c3545dfc5478a9d5731b36e3ce
qoquma <euichan1108@gmail.com>
2021-10-10 02:05:02 +0900

    oauth blacklist

--- trashcanMap/accounts/urls.py
+++ trashcanMap/accounts/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
   path('/google/login', views.google_login),
   path('/google/callback/', views.google_callback,      name='google_callback'),  
   path('/google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
-  path('', include('dj_rest_auth.urls')),
-  path('', include('dj_rest_auth.registration.urls')),
-  path('', include('allauth.urls')),
-  path('', include(router.urls)),
+  path('/', include('dj_rest_auth.urls')),
+  path('/', include('dj_rest_auth.registration.urls')),
+  path('/', include('allauth.urls')),
+  path('/', include(router.urls)),
   #path('', include('accounts.urls')),
 ]
