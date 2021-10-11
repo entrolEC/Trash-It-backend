6c4383a3374a4c789314d7a23bd5fe2c54016ca6
qoquma <euichan1108@gmail.com>
2021-10-12 03:14:15 +0900

    remove sessionauth

--- trashcanMap/trashcanMap/settings.py
+++ trashcanMap/trashcanMap/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+       # 'rest_framework.authentication.SessionAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
 }
