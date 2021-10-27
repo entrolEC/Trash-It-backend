2bd0e4d2383829c312c69babfab042c413f0a21a
INK-Kano <user1205@naver.com>
2021-10-28 01:45:16 +0900

    Change Admin

--- trashcanMap/location/admin.py
+++ trashcanMap/location/admin.py
@@ -1,2 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from location.models import Trashcan, Likes, DisLikes
+
+class LikesInline(admin.StackedInline):
+    model = Likes
+    extra = 0
+
+class DisLikesInline(admin.StackedInline):
+    model = DisLikes
+    extra = 0
+class TrashcanAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'latitude', 'longitude']
+    inlines = [LikesInline, DisLikesInline]
+
+admin.site.register(Trashcan, TrashcanAdmin)
