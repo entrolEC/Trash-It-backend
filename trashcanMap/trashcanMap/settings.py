b4f8d977f1d103a21d867726f4214867e6fcd3f1
INK-Kano <user1205@naver.com>
2021-11-19 14:35:47 +0900

    Chage settings.py For Server

--- trashcanMap/trashcanMap/settings.py
+++ trashcanMap/trashcanMap/settings.py
@@ -73,12 +73,11 @@ for key, value in secrets.items():
 # SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".ap-northeast-2.compute.amazonaws.com",
     ".trashit.space",
-    "*"
 ]
 
 
@@ -193,7 +192,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
