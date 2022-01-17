c19d53424fb9ce0fbe14eb48e94e9a57cc7cb545
euichan2 <euichan1108@gmail.com>
2022-01-17 23:23:12 +0900

    Change URL

---
+++ trashcanMap/location/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from location import views
+from django.conf.urls import url, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'locations', views.TrashcanViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    #path('signup/',views.SignupView.as_view()),
+    #path('signin/',views.SigninView.as_view()),
+    path('action/',views.TrashcanActionViewSet.as_view({'post': 'action'})),
+    path('pin/', views.PinView.as_view({'get': 'list'})),
+    path('check/', views.TrashcanCheckViewSet.as_view({'post': 'is_trashcan'})),
+]
