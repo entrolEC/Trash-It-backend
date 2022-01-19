4d362b3ff05b705414180afc62d89f0141b7830b
PRESSO <caff1nepill@gmail.com>
2022-01-19 17:50:34 +0900

    Fix accounts URL Bug

--- trashcanMap/trashcanMap/urls.py
+++ trashcanMap/trashcanMap/urls.py
@@ -11,7 +11,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('location.urls'))
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
