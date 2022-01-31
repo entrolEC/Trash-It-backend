b8b70f80b524ee52fe2e00fbd1553415a5a9bc4e
PRESSO <caff1nepill@gmail.com>
2022-01-31 15:27:22 +0900

    Change DATABASE To MySQL

--- trashcanMap/trashcanMap/settings.py
+++ trashcanMap/trashcanMap/settings.py
@@ -168,12 +168,6 @@ WSGI_APPLICATION = 'trashcanMap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
